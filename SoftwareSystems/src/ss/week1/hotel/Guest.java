b3b15cd6c2542348112a029a8fe955b90f626622
Wybe <wybe@ruurdwestra.nl>
2018-11-14 14:42:14 +0100

    + Assignment 1.11

---
+++ SoftwareSystems/src/ss/week1/hotel/Guest.java
@@ -0,0 +1,25 @@
+package ss.week1.hotel;
+
+public class Guest {
+	
+	public Guest(String name) {
+		
+	}
+
+	public String getName() {
+		return "";
+	}
+
+	public Room getRoom() {
+		return null;
+	}
+
+	public boolean checkin() {
+		return true;
+	}
+
+	public boolean checkout() {
+		return true;
+	}
+
+}
