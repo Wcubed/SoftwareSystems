00257e65486638f88afbe86e2e40ab290f8e4f7f
Wybe Westra <wybe@ruurdwestra.nl>
2018-11-25 20:19:16 +0100

    - Removed the static INITPASS from the checker implementations.

--- SoftwareSystems/src/ss/week3/pw/BasicChecker.java
+++ SoftwareSystems/src/ss/week3/pw/BasicChecker.java
@@ -1,8 +1,6 @@
 package ss.week3.pw;
 
 public class BasicChecker implements Checker {
-	
-	public static final String INITPASS = "SFDJKLS";
 
 	@Override
 	public String generatePassword() {
--- SoftwareSystems/src/ss/week3/pw/StrongChecker.java
+++ SoftwareSystems/src/ss/week3/pw/StrongChecker.java
@@ -2,8 +2,6 @@ package ss.week3.pw;
 
 public class StrongChecker extends BasicChecker {
 	
-	public static final String INITPASS = "SFDJKLS5";
-	
 	@Override
 	public boolean acceptable(String suggestion) {
 		assert suggestion != null;
