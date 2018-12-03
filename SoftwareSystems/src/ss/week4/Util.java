d21672ffe1cd1acb59941955ad2a10c690f78565
Wybe Westra <wybe@ruurdwestra.nl>
2018-12-03 21:41:10 +0100

    + Satisfied coding conventions in Util.

--- SoftwareSystems/src/ss/week4/Util.java
+++ SoftwareSystems/src/ss/week4/Util.java
@@ -2,32 +2,30 @@ package ss.week4;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.lang.String;
 
 public class Util {
-    public static <Element> List<Element> zip
-            (List<Element> l1, List<Element> l2) {
-        ArrayList<Element> RESULT = new ArrayList<>();
-        for (int i = 0; i < l1.size(); i++) {
-            RESULT.add(l1.get(i));
-            RESULT.add(l2.get(i));
-        }
-        return RESULT;
-    }
+	public static <E> List<E> zip(List<E> l1, List<E> l2) {
+		ArrayList<E> result = new ArrayList<>();
+		for (int i = 0; i < l1.size(); i++) {
+			result.add(l1.get(i));
+			result.add(l2.get(i));
+		}
+		return result;
+	}
 
-    /**
-     * signum function
-     * 
-     * @param i
-     *            the function argument
-     * @return -1, 0 or 1 if the argument is negative, 0 or positive
-     */
-    public static int signum(int i) {
-        if(i < 0)
-            return -1;
-        else if (i > 0)
-            return 1;
-        else
-            return 0;
-    }
+	/**
+	 * signum function.
+	 * 
+	 * @param i the function argument
+	 * @return -1, 0 or 1 if the argument is negative, 0 or positive
+	 */
+	public static int signum(int i) {
+		if (i < 0) {
+			return -1;
+		} else if (i > 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 }
