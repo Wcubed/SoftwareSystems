30e6f6c6ef7239a34a506cab13e44da64a9d81b4
Wybe Westra <wybe@ruurdwestra.nl>
2018-12-13 15:57:53 +0100

    + Implemented EncodingTest.

--- SoftwareSystems/src/ss/week5/EncodingTest.java
+++ SoftwareSystems/src/ss/week5/EncodingTest.java
@@ -2,6 +2,7 @@ package ss.week5;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.binary.Base64;
 
 /**
  * A simple class that experiments with the Hex encoding
@@ -10,8 +11,40 @@ import org.apache.commons.codec.binary.Hex;
  */
 public class EncodingTest {
     public static void main(String[] args) throws DecoderException {
-        String input = "Hello World";
+    	// ---- Hex ----
+    	
+        String input = "Hello Big World";
 
         System.out.println(Hex.encodeHexString(input.getBytes()));
+        
+        byte[] outputHex = Hex.decodeHex("48656c6c6f20576f726c64");
+        System.out.println(new String(outputHex));
+        
+        // ---- Base64 ----
+        
+        input = "Hello World";
+
+        System.out.println(Base64.encodeBase64String(input.getBytes()));
+        
+        byte[] output64 = Base64.decodeBase64("SGVsbG8gV29ybGQ=");
+        System.out.println(new String(output64));
+        
+        // ---- Comparison ----
+        
+        input = "010203040506";
+        System.out.println(Base64.encodeBase64(Hex.decodeHex("48656c6c6f20576f726c64")));
+        
+        // ----
+        
+        System.out.println(new String(Base64.decodeBase64("U29mdHdhcmUgU3lzdGVtcw==")));
+        
+        // ----
+        
+        System.out.println(Base64.encodeBase64String("a".getBytes()));
+        System.out.println(Base64.encodeBase64String("aa".getBytes()));
+        System.out.println(Base64.encodeBase64String("aaa".getBytes()));
+        System.out.println(Base64.encodeBase64String("aaaa".getBytes()));
+        System.out.println(Base64.encodeBase64String("aaaaa".getBytes()));
+        System.out.println(Base64.encodeBase64String("aaaaaa".getBytes()));
     }
 }
