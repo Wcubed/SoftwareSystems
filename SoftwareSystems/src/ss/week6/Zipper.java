952e1912f3af7f5671f8b0eacdfe367ddeb5defd
Wybe Westra <wybe@ruurdwestra.nl>
2018-12-14 07:30:46 +0100

    + Implemented Zip2 and its exceptions.

--- SoftwareSystems/src/ss/week6/Zipper.java
+++ SoftwareSystems/src/ss/week6/Zipper.java
@@ -1,30 +1,46 @@
 package ss.week6;
 
 public class Zipper {
-    /*@
-       requires s1 != null & s2 != null;
-       requires s1.length() == s2.length();
-     */
-    public static String zip(String s1, String s2) {
-        String result = "";
-        for (int i = 0; i < s1.length(); i++) {
-            result += Character.toString(s1.charAt(i))
-                      + Character.toString(s2.charAt(i));
-        }
-        return result;
-    }
-    
+	/*
+	 * @ requires s1 != null & s2 != null; requires s1.length() == s2.length();
+	 */
+	public static String zip(String s1, String s2) {
+		String result = "";
+		for (int i = 0; i < s1.length(); i++) {
+			result += Character.toString(s1.charAt(i)) + Character.toString(s2.charAt(i));
+		}
+		return result;
+	}
 
-    public static void main(String[] args) {
-        String s1 = args.length >= 1 ? args[0] : null;
-        String s2 = args.length >= 2 ? args[1] : null;
-        if (s1 == null || s2 == null) {
-            System.out.println("error: too few command line arguments");
-        } else if (s1.length() != s2.length()) {
-            System.out.println("error: length of command line arguments "
-                    + "differ (" + s1.length() + ", " + s2.length() + ")");
-        } else {
-            System.out.println(zip(s1, s2));
-        }
-    }
+	/*
+	 * @ requires s1 != null & s2 != null; requires s1.length() == s2.length();
+	 */
+	public static String zip2(String s1, String s2)
+			throws TooFewArgumentsException, ArgumentLengthsDifferException {
+		if (s1 == null || s2 == null) {
+			throw new TooFewArgumentsException();
+		} else if (s1.length() != s2.length()) {
+			throw new ArgumentLengthsDifferException();
+		}
+
+		String result = "";
+		for (int i = 0; i < s1.length(); i++) {
+			result += Character.toString(s1.charAt(i)) + Character.toString(s2.charAt(i));
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		String s1 = args.length >= 1 ? args[0] : null;
+		String s2 = args.length >= 2 ? args[1] : null;
+
+		try {
+			System.out.println(zip2(s1, s2));
+		} catch (TooFewArgumentsException e) {
+			System.out.println("error: too few command line arguments");
+		} catch (ArgumentLengthsDifferException e) {
+			System.out.println("error: length of command line arguments " + "differ (" + s1.length()
+					+ ", " + s2.length() + ")");
+		}
+	}
 }
